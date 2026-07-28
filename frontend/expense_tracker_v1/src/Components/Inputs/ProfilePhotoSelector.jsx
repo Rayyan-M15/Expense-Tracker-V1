2e419c2fb25f6cef972908380662696b75f92b6b
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-07-28 22:36:24 +0530

    Profile photo section done halfway

--- frontend/expense_tracker_v1/src/Components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/expense_tracker_v1/src/Components/Inputs/ProfilePhotoSelector.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { LuUser, LuUpload, LuTrash } from 'react-icons/lu';
 
 const ProfilePhotoSelector = ({image, setImage}) => {
-    const intputRef = useRef(null);
-    const [previewUrl,setPreviewUrl] = useState();
+    const inputRef = useRef(null);
+    const [previewUrl,setPreviewUrl] = useState(null);
     const handleImageChange = (event) =>{
-        co
-    }
+        const file= event.target.files[0];
+        if(file){
+            //image update
+
+            setImage(file);
+
+            const preview = URL.createObjectURL(file);
+            setPreviewUrl(preview);
+        }
+    };
+
+    const handleRemoveImage = () =>{
+        setImage(null);
+        setPreviewUrl(null)
+    };
+
+    const onChooseFile = () => {
+        inputRef.current.click();
+    };
     return(
-        <div></div>
+        <div className="flex justify-center mb-6">
+            <input
+                type = "file"
+                accept = "image/*"
+                ref = {inputRef}
+                onChange = {handleImageChange}
+                className = "hidden"
+            />
+
+            {!image? (
+                <div className="">
+                    <LuUser className="" />
+
+                    <button
+                        type = "button"
+                        className=""
+                        onClick= {onChooseFile}
+                    >
+                    
+                        <LuUpload />
+                    </button>
+
+                    
+
+                </div>
+            ):(
+                <div className="">
+                    <img 
+                        src = {previewUrl}
+                        alt = "profile photo"
+                        className= ""
+                    />
+                    <button
+                        type = "button"
+                        className=""
+                        onClick={handleRemoveImage}
+                    >
+                        <LuTrash />
+                    </button>
+                </div>
+            )
+        }
+
+        </div>
     )
+
 }
+export default ProfilePhotoSelector;
